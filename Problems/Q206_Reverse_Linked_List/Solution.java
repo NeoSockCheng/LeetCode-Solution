e692e90802b67b61db03ed268f18e041b90dfc63
Neo <sockchengneo@gmail.com>
2025-02-10 12:32:54 +0800

    10Feb LinkedList

---
+++ Problems/Q206_Reverse_Linked_List/Solution.java
@@ -0,0 +1,36 @@
+package Problems.Q206_Reverse_Linked_List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode temp = head.next;
+        curr.next = prev;
+
+        while (temp != null) {
+            prev = curr;
+            curr = temp;
+            temp = curr.next;
+            curr.next = prev;
+        }
+
+        return curr;
+    }
+}
+
+// Question: reverse linkedlist
+// Approach: use three pointers, prev, curr, and temp
